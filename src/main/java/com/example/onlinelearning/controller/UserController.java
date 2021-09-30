93ca1e8b34e62ae52587e6f1de88be6a9e4203f0
Lâm Bùi <lambthe150099@fpt.edu.vn>
2021-09-30 15:54:08 +0700

    Seperate app controller and user controleer

--- src/main/java/com/example/onlinelearning/controller/UserController.java
+++ src/main/java/com/example/onlinelearning/controller/UserController.java
@@ -9,17 +9,11 @@ import com.example.onlinelearning.service.UserService;
 import com.example.onlinelearning.entity.Role;
 import com.example.onlinelearning.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
@@ -38,13 +32,6 @@ public class UserController {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @GetMapping("/process_register")
-    public String createNewUser(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "sign_up";
-    }
 
     @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute(name = "user") User user, HttpServletRequest request) throws MessagingException, UnsupportedEncodingException {
@@ -120,22 +107,6 @@ public class UserController {
         }
     }
 
-    @GetMapping("/login")
-    public String loginPage() {
-        //prevent user return back to login page if they already login to the system
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
-            return "login";
-        }
-        return "redirect:/";
-    }
 
-    @GetMapping("/verify")
-    public String verifyAccount(@Param("code") String code, Model model) {
-        boolean verified = service.verify(code);
-        String pageTitle = verified ? "Verification Succeeded!" : "Verification Failed";
-        model.addAttribute("pageTitle", pageTitle);
-        return (verified ? "/verify_success" : "/verify_failed");
-    }
 
 }
