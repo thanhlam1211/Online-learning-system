253f42b4d70a75fc819ab4dedab193a3efc98da3
Lâm Bùi <lambthe150099@fpt.edu.vn>
2021-10-26 15:36:41 +0700

    chore: update authorization for admin, teacher

--- src/main/java/com/example/onlinelearning/config/MvcConfig.java
+++ src/main/java/com/example/onlinelearning/config/MvcConfig.java
@@ -13,9 +13,5 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("/index");
         registry.addViewController("/login").setViewName("/login");
-
-        registry.addViewController("/login_success").setViewName("/login_success");
-        registry.addViewController("/login_error").setViewName("/login_error");
-        registry.addViewController("/logout_success").setViewName("/logout_success");
     }
 }
